8c40a233f23e2f46f24776a14349b5f7b16bb34b
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-11-07 17:37:58 -0600

    Add some hibernate mappings to the course entity to fetch your instructor, students and reviews

--- src/main/java/com/makingdevs/springdev/domain/entity/Course.java
+++ src/main/java/com/makingdevs/springdev/domain/entity/Course.java
@@ -4,12 +4,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -25,8 +33,31 @@ public class Course {
   @Column(name = "title", nullable = false)
   private String title;
 
-  public Course(String title) {
+  @ManyToOne(
+    fetch = FetchType.EAGER,
+    cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH}
+  )
+  @JoinColumn(name = "instructor_id")
+  private Instructor instructor;
+
+  @ManyToMany(
+    fetch = FetchType.LAZY,
+    cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH}
+  )
+  @JoinTable(
+    name = "course_student",
+    joinColumns = @JoinColumn(name = "course_id"),
+    inverseJoinColumns = @JoinColumn(name = "student_id")
+  )
+  private List<Student> students;
+
+  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @JoinColumn(name = "course_id")
+  private List<Review> reviews;
+
+  public Course(String title, Instructor instructor) {
     this.title = title;
+    this.instructor = instructor;
   }
 
 }
