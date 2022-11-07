4d885593b5dbee53b8f6128d70e83ac988c5b184
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-11-07 17:48:03 -0600

    Add some hibernate mappings to the instructor entity to fetch your detail and courses

--- src/main/java/com/makingdevs/springdev/domain/entity/Instructor.java
+++ src/main/java/com/makingdevs/springdev/domain/entity/Instructor.java
@@ -3,13 +3,21 @@ package com.makingdevs.springdev.domain.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -26,10 +34,28 @@ public class Instructor {
   @Column(name = "first_name", nullable = false)
   private String firstName;
 
-  @Column(name = "lastName", nullable = false)
+  @Column(name = "last_name", nullable = false)
   private String lastName;
 
   @Column(name = "email", nullable = false)
   private String email;
+
+  @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @Fetch(FetchMode.JOIN)
+  @JoinColumn(name = "instructor_detail_id")
+  private InstructorDetail instructorDetail;
+
+  @OneToMany(
+    fetch = FetchType.LAZY,
+    mappedBy = "instructor",
+    cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH}
+  )
+  private List<Course> courses;
+
+  public Instructor(String firstName, String lastName, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
 
 }
