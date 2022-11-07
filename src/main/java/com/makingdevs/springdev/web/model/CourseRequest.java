cc4207f68a5736f288fd6bd1b0b9812e28b28ff0
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-11-07 17:54:55 -0600

    Update validations of the course request model

--- src/main/java/com/makingdevs/springdev/web/model/CourseRequest.java
+++ src/main/java/com/makingdevs/springdev/web/model/CourseRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class CourseRequest {
@@ -11,5 +12,9 @@ public class CourseRequest {
   @JsonProperty("title")
   @NotBlank
   private String title;
+
+  @JsonProperty("instructor_id")
+  @NotNull
+  private Long instructorId;
 
 }
