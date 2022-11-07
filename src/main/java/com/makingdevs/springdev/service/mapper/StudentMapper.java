783d5d8926b7f00695ed673a9408554965f9515f
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-11-07 17:52:20 -0600

    Add methods to student mapper to map student entities

--- src/main/java/com/makingdevs/springdev/service/mapper/StudentMapper.java
+++ src/main/java/com/makingdevs/springdev/service/mapper/StudentMapper.java
@@ -1,16 +1,51 @@
 package com.makingdevs.springdev.service.mapper;
 
 import com.makingdevs.springdev.domain.entity.Student;
+import com.makingdevs.springdev.service.dto.CourseDto;
+import com.makingdevs.springdev.service.dto.StudentDto;
 import com.makingdevs.springdev.web.model.StudentRequest;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class StudentMapper {
 
   public static Student toEntity(StudentRequest studentRequest) {
     return new Student(
+      studentRequest.getDni(),
       studentRequest.getFirstName(),
       studentRequest.getLastName(),
       studentRequest.getEmail()
     );
+  }
+
+  public static StudentDto toDto(Student student) {
+    return StudentDto.builder()
+      .id(student.getId())
+      .dni(student.getDni())
+      .firstName(student.getFirstName())
+      .lastName(student.getLastName())
+      .email(student.getEmail())
+      .build();
+  }
+
+  public static StudentDto toDetailedDto(Student student) {
+    List<CourseDto> courses = CourseMapper.toDtoList(student.getCourses());
+
+    return StudentDto.builder()
+      .id(student.getId())
+      .dni(student.getDni())
+      .firstName(student.getFirstName())
+      .lastName(student.getLastName())
+      .email(student.getEmail())
+      .courses(courses)
+      .build();
+  }
+
+  public static List<StudentDto> toDtoList(List<Student> students) {
+    return students.stream().map(StudentMapper::toDto).collect(Collectors.toList());
   }
 
 }
