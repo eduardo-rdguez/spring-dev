96cadecaffeb33451e477111ed08b4d0cc664761
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-11-07 17:55:36 -0600

    Reformat main class

--- src/main/java/com/makingdevs/springdev/SpringDevApplication.java
+++ src/main/java/com/makingdevs/springdev/SpringDevApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDevApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDevApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDevApplication.class, args);
+  }
 
 }
