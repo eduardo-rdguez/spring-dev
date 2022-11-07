3b9bbb6205f13b7c59fb8ed901cb1e5962dd8221
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-11-07 17:52:46 -0600

    Add instructor mapper

---
+++ src/main/java/com/makingdevs/springdev/service/mapper/InstructorMapper.java
@@ -0,0 +1,57 @@
+package com.makingdevs.springdev.service.mapper;
+
+import com.makingdevs.springdev.domain.entity.Instructor;
+import com.makingdevs.springdev.domain.entity.InstructorDetail;
+import com.makingdevs.springdev.service.dto.CourseDto;
+import com.makingdevs.springdev.service.dto.InstructorDto;
+import com.makingdevs.springdev.web.model.InstructorRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class InstructorMapper {
+
+  public static Instructor toEntity(
+    InstructorRequest instructorRequest,
+    InstructorDetail instructorDetail
+  ) {
+    Instructor instructor = new Instructor(
+      instructorRequest.getFirstName(),
+      instructorRequest.getLastName(),
+      instructorRequest.getEmail()
+    );
+    instructor.setInstructorDetail(instructorDetail);
+
+    return instructor;
+  }
+
+  public static InstructorDto toDto(Instructor instructor) {
+    return InstructorDto.builder()
+      .id(instructor.getId())
+      .firstName(instructor.getFirstName())
+      .lastName(instructor.getLastName())
+      .email(instructor.getEmail())
+      .birthdate(instructor.getInstructorDetail().getBirthdate())
+      .bloodType(instructor.getInstructorDetail().getBloodType())
+      .build();
+  }
+
+  public static InstructorDto toDetailedDto(Instructor instructor) {
+    List<CourseDto> courses = CourseMapper.toDtoList(instructor.getCourses());
+
+    return InstructorDto.builder()
+      .id(instructor.getId())
+      .firstName(instructor.getFirstName())
+      .lastName(instructor.getLastName())
+      .email(instructor.getEmail())
+      .courses(courses)
+      .build();
+  }
+
+  public static List<InstructorDto> toDtoList(List<Instructor> instructors) {
+    return instructors.stream().map(InstructorMapper::toDto).collect(Collectors.toList());
+  }
+
+}
