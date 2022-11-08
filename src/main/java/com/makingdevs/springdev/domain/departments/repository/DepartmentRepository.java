6fcd6cdd23605d89d5a6835c4b986a4b1276d86c
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-11-08 13:02:02 -0600

    Add JPQL query to find department by name

--- src/main/java/com/makingdevs/springdev/domain/departments/repository/DepartmentRepository.java
+++ src/main/java/com/makingdevs/springdev/domain/departments/repository/DepartmentRepository.java
@@ -2,11 +2,14 @@ package com.makingdevs.springdev.domain.departments.repository;
 
 import com.makingdevs.springdev.domain.departments.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
-  Optional<Department> findByNameIgnoreCase(String name);
+  @Query("SELECT d FROM Department d WHERE d.name = :name")
+  Optional<Department> findByNameIgnoreCase(@Param("name") String name);
 
 }
